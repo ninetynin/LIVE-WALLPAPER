28ffb773d6c26295158c00ed77a0b5fcaf017854
ninetynin <wekandmusic@gmail.com>
2022-10-14 20:07:45 +0530

    Added successful test files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// mod createDir;
+// mod windowsservice;
+// mod setwallpaper;
+
 fn main() {
-    println!("Hello, world!");
+    // ------------------ Create Require Dirs. ------------------
+    // createDir::create_all_dirs(); 
+    // ----`----`----`----`----`----`----`----`----`----`----`----
+    // ------------------ Set Wallpaper. ------------------------
+
+    // ----`----`----`----`----`----`----`----`----`----`----`----
+    // ------------------ Start Service. ------------------------
+    // windowsservice::start_service();
+    // set it to run on startup
+    // some resources to check out : / ../servicecheck.md
+    // ----`----`----`----`----`----`----`----`----`----`----`----
 }
